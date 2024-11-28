a55a5ee79259facb9dfdfc3e27a2cdd0acfde9a6
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2024-11-28 16:10:56 +0900

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void generateFibonacci(int n) {
+    int t1 = 0, t2 = 1, nextTerm;
+
+    printf("Fibonacci Sequence: ");
+
+    for (int i = 1; i <= n; ++i) {
+        printf("%d ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter the number of terms (1-47): ");
+    scanf("%d", &n);
+
+    if (n < 1 || n > 47) {
+        printf("Error: Input must be a natural number between 1 and 47\n");
+    return 1;
+    }
+
+    if (n >= 48) {
+        printf("Overflow: Input too large\n");
+        return 1;
+    }
+    else {
+        generateFibonacci(n);
+    }
+
+    return 0;
+}
+// fibonacci.h
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+
+void generateFibonacci(int n);
+
+#endif // FIBONACCI_H
